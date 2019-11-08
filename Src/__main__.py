e73794526059ef5b009ec9c2a0bd12da9ad1f151
Bert.Braun <bert.braun@feig.de>
2019-11-08 15:13:46 +0100

    Glitch test added

--- Src/__main__.py
+++ Src/__main__.py
@@ -10,6 +10,8 @@ PSUBAUD = 460800
 HCRDEV = "/dev/ttyUSB0"
 HCRBAUD = 115200
 
+NORMVOLT = 5.2
+NORMAMP = 1.0
 
 serpsu = Gpib.openCom(PSUDEV, PSUBAUD)
 print ("GPIB-Adapter:    {}".format(serpsu.name))
@@ -21,25 +23,27 @@ print ("PSU-Ident:       {}\n".format(E3631A.getVersion(serpsu, GPIBADDR)))
 Gpib.setauto(serpsu, 0)
 
 
-E3631A.setoutput(serpsu, 0, 0.0, 1.0)
+# Ausgangsspannung 0V, Ausgang aktiv
+E3631A.setoutput(serpsu, 0, NORMVOLT, NORMAMP)
 E3631A.enableoutput(serpsu, 1)
 
 while 1:
     E3631A.clearerrs(serpsu, GPIBADDR)
-    E3631A.vramp(serpsu, 0, 0.0, 5.2, 0.2)
+    #E3631A.vramp(serpsu, 0, 0.0, 5.2, 0.2)
     #E3631A.setoutput(serpsu, 0, 5.1, 1.0)
-    time.sleep(2)
+    time.sleep(2) # evtl. Hochlauf des Lesers abwarten
     serhcr = Shcr.openCom(HCRDEV, HCRBAUD)
     if serhcr:
         resp = Shcr.getsecstatus(serhcr)
-        Shcr.clearsecstatus(serhcr)
-        Shcr.closecom(serhcr)
-        if Shcr.decodestatus(resp):
+        if Shcr.decodestatus(resp): # wenn ein richtiger Tamper vorliegt
             break
+        Shcr.clearsecstatus(serhcr) # sonst Register löschen (VddLo)
+        Shcr.closecom(serhcr)
     time.sleep(1)
-    #E3631A.setoutput(serpsu, 0, 4.0, 1.0)
-    E3631A.vramp(serpsu, 0, 5.2, 0.0, 0.2)
-    time.sleep(2)
+    E3631A.setoutput(serpsu, 0, 3.0, NORMAMP) # Glitch erzeugen
+    #time.sleep(0.1)
+    E3631A.setoutput(serpsu, 0, NORMVOLT, NORMAMP)
+    #E3631A.vramp(serpsu, 0, 5.2, 0.0, 0.2)
 
 E3631A.enableoutput(serpsu, 0)
 serpsu.close()
